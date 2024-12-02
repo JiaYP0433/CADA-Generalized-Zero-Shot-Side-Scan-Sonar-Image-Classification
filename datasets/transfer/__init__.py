8bcc32d92225fed5bc03b707a4ce00625f8ce5b9
JiaYP0433 <117506164+JiaYP0433@users.noreply.github.com>
2024-12-02 16:50:57 +0800

    Add files via upload

--- datasets/transfer/__init__.py
+++ datasets/transfer/__init__.py
@@ -1,8 +1,8 @@
-import argparse
 from .photowct import PhotoWCT
 from .liwct import SonarNoiseWCT
 from .stytr import StyTrans
 from .asepa import AsePaTrans
+from .gcea import GCEATrans
 from .ccpl import CplTrans
 
 transfer_list = {
@@ -10,9 +10,10 @@ transfer_list = {
     "liwct": SonarNoiseWCT,
     "stytr": StyTrans,
     "asepa": AsePaTrans,
+    "gcea": GCEATrans,
     "ccpl": CplTrans,
 }
 
 
 def build_transfer(style_mode, mode='none'):
-    return transfer_list[style_mode](mode=mode)    # cfg, style_mode=style_mode
+    return transfer_list[style_mode](mode=mode) 
